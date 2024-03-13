21456cdfb31fcd1bc103dc87da877c01735ff321
chenZL <czl970721@gmail.com>
2024-03-12 23:23:27 -0400

    rm useless

--- app/main.py
+++ app/main.py
@@ -15,14 +15,12 @@ def main():
 def request_handler(sock: socket.socket) -> None:
     while True:
         request_bytes = sock.recv(1024)
-
+        print(request_bytes)
         request_value = RedisValue.from_bytes(request_bytes)
-        response_count = len(request_value.value[0].value.split("\n"))
 
         response_value = RedisSimpleString("PONG")
 
-        for _ in range(response_count):
-            sock.send(response_value.deserialize())
+        sock.send(response_value.deserialize())
 
 
 if __name__ == "__main__":
